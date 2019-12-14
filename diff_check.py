bb376f4def025d0724c5134a8f564e77c4ffce1f
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-14 00:35:58 +0000

    add script for neutralizing bias demo

--- diff_check.py
+++ diff_check.py
@@ -1,5 +1,5 @@
 from difflib import SequenceMatcher, Differ
 text1 = open("test1.txt").read()
-text2 = open("test2.txt").read()
+text2 = open("text2.txt").read()
 m = SequenceMatcher(None, text1, text2)
 print(m.ratio())

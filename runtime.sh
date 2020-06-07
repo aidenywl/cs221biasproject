cecbb75f3434184275e7606c9e6df42f33a8a6a2
Aiden Low <yewwoei.low@u.nus.edu>
2020-06-07 10:38:05 +0000

    add configuration for model REST api

--- runtime.sh
+++ runtime.sh
@@ -4,7 +4,7 @@ while true
 do
 	read -p "Enter the sentence you want to neutralize subjective bias from: " sent
 	echo $sent > tmp.txt
-	python OpenNMT-py/translate.py -model _step_67260.pt -src tmp.txt -output pred.txt -replace_unk -verbose
+	python OpenNMT-py/translate.py -model standard_32000_brnn_seed-1_step_84075_epoch_25.pt -src tmp.txt -output pred.txt -replace_unk -verbose
         cat pred.txt
 done	
 
